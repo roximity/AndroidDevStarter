50a6dac8f37dc93258359f4e179e17bf46aca858
Cole <g.a.cole.richards@gmail.com>
2016-06-09 10:36:07 -0600

    Starting ROXIMITYEngine in ROXIMITYObserver class

--- app/src/main/java/com/example/roximity/androiddevstarter/MainActivity.java
+++ app/src/main/java/com/example/roximity/androiddevstarter/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.roximity.androiddevstarter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.roximity.system.exceptions.GooglePlayServicesMissingException;
+import com.roximity.system.exceptions.IncorrectContextException;
+import com.roximity.system.exceptions.MissingApplicationIdException;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MAIN_ACTIVITY";
+    private ROXIMITYObserver roximityObserver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupROXObserver();
+
+    }
+
+    private void setupROXObserver(){
+        try {
+            this.roximityObserver = new ROXIMITYObserver(this.getApplicationContext());
+        } catch (GooglePlayServicesMissingException e) {
+            Log.d(TAG, "Couldn't start ROXIMITY due to GooglePlayServices Error");
+            e.printStackTrace();
+        } catch (IncorrectContextException e) {
+            Log.d(TAG, "Couldn't start ROXIMITY due to incorrect context Error");
+            e.printStackTrace();
+        } catch (MissingApplicationIdException e) {
+            Log.d(TAG, "Couldn't start ROXIMITY due to missing application id Error");
+            e.printStackTrace();
+        }
     }
 }
---
+++ app/src/main/java/com/example/roximity/androiddevstarter/ROXIMITYObserver.java
@@ -0,0 +1,41 @@
+package com.example.roximity.androiddevstarter;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.roximity.sdk.ROXIMITYEngine;
+import com.roximity.sdk.ROXIMITYEngineListener;
+import com.roximity.sdk.external.ROXConsts;
+import com.roximity.system.exceptions.GooglePlayServicesMissingException;
+import com.roximity.system.exceptions.IncorrectContextException;
+import com.roximity.system.exceptions.MissingApplicationIdException;
+
+import java.util.HashMap;
+
+/**
+ * Created by colerichards on 6/9/16.
+ */
+public class ROXIMITYObserver implements ROXIMITYEngineListener {
+
+
+    private static final String TAG = "ROXObserver";
+
+    public ROXIMITYObserver(Context context) throws GooglePlayServicesMissingException, IncorrectContextException, MissingApplicationIdException {
+        HashMap<String,Object> roximityOptions = new HashMap<>();
+        roximityOptions.put(ROXConsts.ENGINE_OPTIONS_MUTE_BLUETOOTH_OFF_ALERT, false);
+        roximityOptions.put(ROXConsts.ENGINE_OPTIONS_MUTE_REQUEST_ALERTS, false);
+        roximityOptions.put(ROXConsts.ENGINE_OPTIONS_START_LOCATION_DEACTIVATED, false);
+
+        ROXIMITYEngine.startEngineWithOptions(context, R.drawable.ic_launcher, roximityOptions, this, null);
+    }
+
+    @Override
+    public void onROXIMITYEngineStarted() {
+        Log.d(TAG, "ROXIMITYEngine has started");
+    }
+
+    @Override
+    public void onROXIMITYEngineStopped() {
+        Log.d(TAG, "ROXIMITYEngine was stopped");
+    }
+}
